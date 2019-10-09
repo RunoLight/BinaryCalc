10fc6797194704614968b2aab5ca3ce3eac30349
RunoLight <vasiacotic@gmail.com>
2019-10-09 10:05:26 +1000

    A line working

--- MainForm.cs
+++ MainForm.cs
@@ -20,14 +20,15 @@ namespace BinaryCalc
         int Notation = 10;
         //*
 
-        private void MainForm_Load(object sender, EventArgs e)
+        Calculus.Term variableA = new Calculus.Term();
+        Calculus.Term variableB = new Calculus.Term();
+        Calculus.Term variableC = new Calculus.Term();
+       
+
+        public void MainForm_Load(object sender, EventArgs e)
         {
-            Calculus.Term variableA = new Calculus.Term();
-            Calculus.Term variableB = new Calculus.Term();
-            Calculus.Term variableC = new Calculus.Term();
             variableA.denary = 0; variableB.denary = 0; variableC.denary = 0;
             variableA.RefreshBy(10); variableB.RefreshBy(10); variableC.RefreshBy(10);
-
             Anotation.Text = Convert.ToString(Notation);
         }
 
@@ -72,6 +73,8 @@ namespace BinaryCalc
             {
                 clickedButton.Text = "0";
             }
+
+            RefreshValue(sender, e);
         }
 
         private void SwitchNotation(object sender, EventArgs e)
@@ -84,6 +87,41 @@ namespace BinaryCalc
                 Notation = 10;
 
             Anotation.Text = Convert.ToString(Notation);
+
+            RefreshValue(sender, e);
+        }
+
+        private void RefreshValue(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+
+            //string temp = string.Empty;
+
+            
+
+            if (clickedButton.Name.StartsWith("A"))
+            {
+                string temp = A15.Text + A14.Text + A13.Text + A12.Text + A11.Text + A10.Text + A9.Text + A8.Text +
+                    A7.Text + A6.Text + A5.Text + A4.Text + A3.Text + A2.Text + A1.Text + A0.Text;
+
+                variableA.binary = temp;
+                variableA.RefreshBy(2);
+
+                if (Notation == 8)
+                    Afull.Text = variableA.octal;
+                else if (Notation == 10)
+                    Afull.Text = Convert.ToString(variableA.denary);
+                else if (Notation == 16)
+                    Afull.Text = variableA.hexadecimal.ToUpper();
+            }
+            else if (clickedButton.Name.StartsWith("B"))
+            {
+
+            }
+            else // C denary button
+            {
+
+            }
         }
     }
 }
